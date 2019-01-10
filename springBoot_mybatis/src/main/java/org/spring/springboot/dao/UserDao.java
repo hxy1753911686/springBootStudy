7e662beef686a5758d775ee1459b6c85d12f3982
hanxiangyu <hxy150326@126.com>
2019-01-10 18:55:01 +0800

    修改@Select

--- springBoot_mybatis/src/main/java/org/spring/springboot/dao/UserDao.java
+++ springBoot_mybatis/src/main/java/org/spring/springboot/dao/UserDao.java
@@ -5,8 +5,8 @@ import org.spring.springboot.entity.User;
 
 @Mapper //標誌為mybatis的mapper，不用xml方式
 public interface UserDao {
-    //select中標註要使用的select語句
-    @Select("select * from city")
+    //select中標註要使用的sql語句,與xml中的sql語句一致
+    @Select("select * from springbootdb.user where user_name = #{userName}")
     //對應xml中的resultMap
     @Results(value = {
             @Result(property = "id",column = "id"),
