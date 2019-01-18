3e5dcf92bbc5cbefee35947cbb105365374e5c8a
hanxiangyu <hxy150326@126.com>
2019-01-18 18:25:40 +0800

    添加restful風格，完善功能

--- springBoot_mybatis/src/main/java/org/spring/springboot/controller/CityController.java
+++ springBoot_mybatis/src/main/java/org/spring/springboot/controller/CityController.java
@@ -5,10 +5,7 @@ import org.spring.springboot.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -21,7 +18,9 @@ public class CityController {
     private CityService cityService;
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public String gotoIndex(HashMap<String, Object> map) {
+    public String gotoIndex(Model model) {
+        List<City> list = cityService.findAllCity();
+        model.addAttribute("cityList", list);
         return "/showCity";
     }
 
@@ -44,13 +43,36 @@ public class CityController {
     @RequestMapping(value = "findCity", method = RequestMethod.POST)
     public String findCityById(@RequestParam(value = "cityId") Long cityId, Model model) {
         List<City> list = new ArrayList<City>();
-        if(cityId != null){
+        if (cityId != null) {
             City city = cityService.findCityById(cityId);
             list.add(city);
-        }else {
+        } else {
             list = cityService.findAllCity();
         }
 
+        model.addAttribute("cityList", list);
+        return "/showCity";
+    }
+
+    @RequestMapping(value = "deleteCity/{id}", method = RequestMethod.POST)
+    public String deleteCityById(@PathVariable("id") Long cityId, Model model) {
+        cityService.deleteCity(cityId);
+        List<City> list = cityService.findAllCity();
+        model.addAttribute("cityList", list);
+        return "/showCity";
+    }
+
+    @RequestMapping(value = "updateCity/{id}", method = RequestMethod.POST)
+    public String toUpdateCity(@PathVariable("id") Long cityId, Model model) {
+        City city = cityService.findCityById(cityId);
+        model.addAttribute("city", city);
+        return "/updateCity";
+    }
+
+    @RequestMapping(value = "updateCity", method = RequestMethod.POST)
+    public String updateCity(@ModelAttribute City city, Model model) {
+        cityService.updateCity(city);
+        List<City> list = cityService.findAllCity();
         model.addAttribute("cityList", list);
         return "/showCity";
     }
